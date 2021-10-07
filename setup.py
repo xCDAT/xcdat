0d6cf4cdc18a351afb2313ea16a9dc06f7f75864
Tom Vo <tomvothecoder@gmail.com>
2021-10-07 14:11:09 -0700

    Bump to 0.1.0rc1

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requires,
     url="https://github.com/XCDAT/xcdat",
-    version="0.1.0",
+    version="0.1.0rc1",
     zip_safe=False,
 )
--- xcdat/__init__.py
+++ xcdat/__init__.py
@@ -2,4 +2,4 @@
 from xcdat.dataset import decode_time_units, open_dataset, open_mfdataset  # noqa: F401
 from xcdat.xcdat import XCDATAccessor  # noqa: F401
 
-__version__ = "0.1.0"
+__version__ = "0.1.0rc1"
