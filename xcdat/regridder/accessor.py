affebbcbe7c5b708c880fc9716c797bd61fcd23c
Jason Boutte <boutte.jason@gmail.com>
2021-11-12 17:10:22 -0800

    Fixes explicitily naming available options for xesmf regridder.

--- xcdat/regridder/accessor.py
+++ xcdat/regridder/accessor.py
@@ -29,7 +29,7 @@ class DatasetRegridderAccessor:
 
         Supported tools:
 
-        - xESMF
+        - xESMF (https://pangeo-xesmf.readthedocs.io/en/latest/)
 
         Parameters
         ----------
--- xcdat/regridder/xesmf.py
+++ xcdat/regridder/xesmf.py
@@ -7,14 +7,73 @@ from xcdat import dataset
 from xcdat.regridder.base import BaseRegridder
 
 
+VALID_METHODS = [
+    "bilinear",
+    "conservative",
+    "conservative_normed",
+    "patch",
+    "nearest_s2d",
+    "nearest_d2s",
+]
+
+VALID_EXTRAP_METHODS = ["inverse_dist", "nearest_s2d"]
+
+
 class XESMFRegridder(BaseRegridder):
+    """Wrapper class for xESMF regridder class.
+
+    Creates a reusable regridder object.
+
+    Parameters
+    ----------
+    src_grid : xr.Dataset
+        Contains source grid coordinates.
+    dst_grid : xr.Dataset
+        Contains desintation grid coordinates.
+    method : str
+        Regridding method. Options are
+        - bilinear
+        - conservative
+        - conservative_normed
+        - patch
+        - nearest_s2d
+        - nearest_d2s
+    data_var : str
+        Target variable to apply regridding to.
+    periodic : bool
+        Treat longitude as periodic. Used for global grids.
+    extrap_method : str
+        Extrapolation method. Options are
+        - inverse_dist
+        - nearest_s2d
+    extrap_dist_exponent : float
+        The exponent to raise the distance to when calculating weights for the extrapolation method.
+    extrap_num_src_pnts : int
+        The number of source points to use for the extrapolation methods that use more than one source point.
+
+    Raises
+    ------
+    KeyError
+        If data variable does not exist in the Dataset.
+    ValueError
+        If `method` is not valid.
+    ValueError
+        If `extrap_method` is not valid.
+
+    Examples
+    --------
+    """
+
     def __init__(
         self,
         src_grid: xr.Dataset,
         dst_grid: xr.Dataset,
         method: str,
         data_var: str = None,
-        **options: Any,
+        periodic: bool = False,
+        extrap_method: str = None,
+        extrap_dist_exponent: float = None,
+        extrap_num_src_pnts: int = None,
     ):
         self._src_grid = src_grid
         self._dst_grid = dst_grid
@@ -29,7 +88,25 @@ class XESMFRegridder(BaseRegridder):
                     f"The data variable {data_var!r} does not exist in the dataset."
                 )
 
-        self._regridder = xe.Regridder(src_var, self._dst_grid, method)
+        if method not in VALID_METHODS:
+            raise ValueError(
+                f"{method!r} is not valid, possible options: {', '.join(VALID_METHODS)}"
+            )
+
+        if extrap_method is not None and extrap_method not in VALID_EXTRAP_METHODS:
+            raise ValueError(
+                f"{extrap_method!r} is not valid, possible options: {', '.join(VALID_EXTRAP_METHODS)}"
+            )
+
+        self._regridder = xe.Regridder(
+            src_var,
+            self._dst_grid,
+            method,
+            periodic=periodic,
+            extrap_method=extrap_method,
+            extrap_dist_exponent=extrap_dist_exponent,
+            extrap_num_src_pnts=extrap_num_src_pnts,
+        )
 
     def regrid(self, ds: xr.Dataset) -> xr.Dataset:
         return self._regridder(ds)
