a7372bda91aaaceaca8b6ef3cf7e740e3fbfba50
Jason Boutte <boutte.jason@gmail.com>
2022-02-23 16:07:24 -0800

    Fixes failing test

--- tests/test_xcdat.py
+++ tests/test_xcdat.py
@@ -24,13 +24,13 @@ class TestXCDATAccessor:
         self.ds.xcdat._dataset.identical(self.ds)
 
     def test_regrid(self):
-        ds = self.ds.copy()
+        ds = self.ds_with_bnds.copy()
 
         out_grid = grid.create_uniform_grid(-90, 90, 4.0, -180, 180, 5.0)
 
         result = ds.xcdat.regrid("ts", out_grid, "xesmf", method="bilinear")
 
-        assert result.shape == (12, 45, 72)
+        assert result.ts.shape == (12, 45, 72)
 
     def test_weighted_spatial_average_for_lat_and_lon_region(self):
         ds = self.ds_with_bnds.copy()
