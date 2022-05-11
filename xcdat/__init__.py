3ad8cabb8991f5a55a7785f7c58659397b937db8
Jason Boutte <boutte.jason@gmail.com>
2022-05-11 09:42:58 -0700

    Exposes grid creation functions to top-level module

--- xcdat/__init__.py
+++ xcdat/__init__.py
@@ -3,6 +3,12 @@ from xcdat.axis import swap_lon_axis  # noqa: F401
 from xcdat.bounds import BoundsAccessor  # noqa: F401
 from xcdat.dataset import decode_non_cf_time, open_dataset, open_mfdataset  # noqa: F401
 from xcdat.regridder.accessor import RegridderAccessor  # noqa: F401
+from xcdat.regridder.grid import (  # noqa: F401
+    create_gaussian_grid,
+    create_global_mean_grid,
+    create_uniform_grid,
+    create_zonal_grid,
+)
 from xcdat.spatial import SpatialAccessor  # noqa: F401
 from xcdat.temporal import TemporalAccessor  # noqa: F401
 from xcdat.utils import compare_datasets  # noqa: F401
