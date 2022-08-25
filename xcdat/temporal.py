fix incorrect results for group averaging with missing data (#320)
* initial fix for #319 
* Refactor `_group_average()`
- Preserve data variable attributes using `xr.set_options(keep_attrs=True)`
- Reuse `self._labeled_time` if it is already set in a previous call to `_group_data()`
- Update group average tests to check data variable test attr is preserved

Co-authored-by: Tom Vo <tomvothecoder@gmail.com>

xcdat/temporal.py
@@ -921,8 +921,23 @@ class TemporalAccessor:
 
         if self._weighted:
             self._weights = self._get_weights()
+            # Weight the data variable.
             dv *= self._weights
-            dv = self._group_data(dv).sum()
+
+            # Ensure missing data (`np.nan`) receives no weight (zero). To
+            # achieve this, first broadcast the one-dimensional (temporal
+            # dimension) shape of the `weights` DataArray to the
+            # multi-dimensional shape of its corresponding data variable.
+            weights, _ = xr.broadcast(self._weights, dv)
+            weights = xr.where(np.isnan(dv), 0.0, weights)
+
+            # Perform weighted average using the formula
+            # WA = sum(data*weights) / sum(weights). The denominator must be
+            # included to take into account zero weight for missing data.
+            with xr.set_options(keep_attrs=True):
+                dv = self._group_data(dv).sum() / self._group_data(weights).sum()
+            # Restore the data variable's name.
+            dv.name = data_var.name
         else:
             dv = self._group_data(dv).mean()
 
@@ -931,7 +946,7 @@ class TemporalAccessor:
         # For example, grouping on "year_season" replaces the time dimension
         # with "year_season". This dimension needs to be renamed back to
         # the original time dimension name before the data variable is added
-        # back to the dataset so that the CF compliant name is maintained.
+        # back to the dataset so that the original name is preserved.
         dv = dv.rename({self._labeled_time.name: self._dim})
 
         # After grouping and aggregating, the grouped time dimension's
@@ -1021,7 +1036,12 @@ class TemporalAccessor:
         if self._mode == "average":
             dv_gb = dv.groupby(f"{self._dim}.{self._freq}")
         else:
-            self._labeled_time = self._label_time_coords(dv[self._dim])
+            # Reuse the labeled time coordinates if they are already generated.
+            # This specifically applies when generating weights, which calls
+            # this method again after it has already been called to group
+            # the data variable with the same time coordinates.
+            if not hasattr(self, "_labeled_time"):
+                self._labeled_time = self._label_time_coords(dv[self._dim])
             dv.coords[self._labeled_time.name] = self._labeled_time
             dv_gb = dv.groupby(self._labeled_time.name)
 