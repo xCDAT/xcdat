67ab89c4cd56bf65499c8f9a0d640b598e0c59df
Tom Vo <tomvothecoder@gmail.com>
2022-09-14 14:01:33 -0700

    Revert default to xesmf

--- xcdat/regridder/accessor.py
+++ xcdat/regridder/accessor.py
@@ -197,7 +197,7 @@ class RegridderAccessor:
         self,
         data_var: str,
         output_grid: xr.Dataset,
-        tool: RegridTool,
+        tool: RegridTool = "xesmf",
         **options: Dict[str, Any],
     ) -> xr.Dataset:
         """
