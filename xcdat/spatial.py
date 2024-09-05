7c27e6f2a7475898c28fc71f1ba83f892fe24c4a
Tom Vo <tomvothecoder@gmail.com>
2024-09-05 11:29:33 -0700

    Fix `TypeError` for optional `region` arg in `TypedDict`

--- xcdat/spatial.py
+++ xcdat/spatial.py
@@ -8,6 +8,7 @@ from typing import (
     Hashable,
     List,
     Literal,
+    Optional,
     Tuple,
     TypedDict,
     Union,
@@ -263,7 +264,7 @@ class SpatialAccessor:
         and pressure).
         """
         Bounds = TypedDict(
-            "Bounds", {"weights_method": Callable, "region": np.ndarray | None}
+            "Bounds", {"weights_method": Callable, "region": Optional[np.ndarray]}
         )
 
         axis_bounds: Dict[SpatialAxis, Bounds] = {
