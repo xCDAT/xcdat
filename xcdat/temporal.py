9669dfc7a10eb81035986957aa29e3dc083efadd
Tom Vo <tomvothecoder@gmail.com>
2024-09-23 10:57:07 -0700

    Convert `self.dim` to a str to fix mypy warnings - Remove unused type ignore comments

--- xcdat/temporal.py
+++ xcdat/temporal.py
@@ -815,7 +815,7 @@ class TemporalAccessor:
         # it becomes obsolete after the data variable is averaged. When the
         # averaged data variable is added to the dataset, the new time dimension
         # and its associated coordinates are also added.
-        ds = ds.drop_dims(self.dim)  # type: ignore
+        ds = ds.drop_dims(self.dim)
         ds[dv_avg.name] = dv_avg
 
         if keep_weights:
@@ -847,7 +847,7 @@ class TemporalAccessor:
         dv = _get_data_var(self._dataset, data_var)
 
         self.data_var = data_var
-        self.dim = get_dim_coords(dv, "T").name
+        self.dim = str(get_dim_coords(dv, "T").name)
 
         if not _contains_datetime_like_objects(dv[self.dim]):
             first_time_coord = dv[self.dim].values[0]
@@ -1119,9 +1119,7 @@ class TemporalAccessor:
         -------
         xr.Dataset
         """
-        ds = ds.sel(  # type: ignore
-            **{self.dim: ~((ds.time.dt.month == 2) & (ds.time.dt.day == 29))}
-        )
+        ds = ds.sel(**{self.dim: ~((ds.time.dt.month == 2) & (ds.time.dt.day == 29))})
         return ds
 
     def _average(self, ds: xr.Dataset, data_var: str) -> xr.DataArray:
@@ -1146,9 +1144,9 @@ class TemporalAccessor:
                 time_bounds = ds.bounds.get_bounds("T", var_key=data_var)
                 self._weights = self._get_weights(time_bounds)
 
-                dv = dv.weighted(self._weights).mean(dim=self.dim)  # type: ignore
+                dv = dv.weighted(self._weights).mean(dim=self.dim)
             else:
-                dv = dv.mean(dim=self.dim)  # type: ignore
+                dv = dv.mean(dim=self.dim)
 
         dv = self._add_operation_attrs(dv)
 
