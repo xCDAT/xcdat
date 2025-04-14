1140e36224cd64b18954f79f79afa3e12df17b8b
Tom Vo <tomvothecoder@gmail.com>
2025-04-14 13:57:24 -0700

    Bump to 0.9.0

--- xcdat/__init__.py
+++ xcdat/__init__.py
@@ -24,4 +24,4 @@ from xcdat.spatial import SpatialAccessor  # noqa: F401
 from xcdat.temporal import TemporalAccessor  # noqa: F401
 from xcdat.utils import compare_datasets  # noqa: F401
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
