a67f12bc70a04d8ec3515ef012028fdd91c58d73
Jason Boutte <boutte3@llnl.gov>
2025-08-01 18:20:48 -0700

    Changes function names and updates docstrings

--- xcdat/__init__.py
+++ xcdat/__init__.py
@@ -10,6 +10,7 @@ from xcdat.axis import (  # noqa: F401
 )
 from xcdat.bounds import BoundsAccessor, create_bounds  # noqa: F401
 from xcdat.dataset import decode_time, open_dataset, open_mfdataset  # noqa: F401
+from xcdat.mask import MaskAccessor  # noqa: F401
 from xcdat.regridder.accessor import RegridderAccessor  # noqa: F401
 from xcdat.regridder.grid import (  # noqa: F401
     create_axis,
@@ -24,6 +25,5 @@ from xcdat.regridder.grid import (  # noqa: F401
 from xcdat.spatial import SpatialAccessor  # noqa: F401
 from xcdat.temporal import TemporalAccessor  # noqa: F401
 from xcdat.utils import compare_datasets  # noqa: F401
-from xcdat.mask import MaskAccessor  # noqa: F401
 
 __version__ = "0.10.0"
